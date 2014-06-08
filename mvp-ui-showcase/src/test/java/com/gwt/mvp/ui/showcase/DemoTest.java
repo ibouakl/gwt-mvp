22f90890d5bb8e57f23e674008f586faa9dfda2f
ibouakl <imad.bouakl@gmail.com>
2014-06-08 16:49:52 +0200

    up

---
+++ mvp-ui-showcase/src/test/java/com/gwt/mvp/ui/showcase/DemoTest.java
@@ -0,0 +1,14 @@
+package com.gwt.mvp.ui.showcase;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DemoTest {
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+}
